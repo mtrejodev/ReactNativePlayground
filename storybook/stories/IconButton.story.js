6e0962b8981d49e1b127089c6d2a736bbba5d0b1
Mauricio Trejo <mauriciotrejo.013@gmail.com>
2020-09-22 16:46:12 -0600

    Storybook configured

---
+++ storybook/stories/IconButton.story.js
@@ -0,0 +1,21 @@
+
+import React from "react";
+import { View } from "react-native";
+import IconButton from "../../src/components/IconButton";
+import { storiesOf } from "@storybook/react-native";
+
+storiesOf("IconButton", module)
+    .add("on", () => (
+        <IconButton
+            icon={"heart"}
+            color={"#333"}
+            onPress={() => console.log("un-favorited!")}
+        />
+    ))
+    .add("off", () => (
+        <IconButton
+            icon={"heart-o"}
+            color={"#333"}
+            onPress={() => console.log("favorited!")}
+        />
+    ));
